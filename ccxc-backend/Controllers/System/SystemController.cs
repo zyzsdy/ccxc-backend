7219ffc6083a1da0f224f4e77598b74715386a49
Ted Zyzsdy <zyzsdy@gmail.com>
2020-05-29 00:33:11 +0800

    首页

---
+++ ccxc-backend/Controllers/System/SystemController.cs
@@ -0,0 +1,23 @@
+﻿using Ccxc.Core.HttpServer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ccxc_backend.Controllers.System
+{
+    [Export(typeof(HttpController))]
+    public class SystemController : HttpController
+    {
+        [HttpHandler("POST", "/get-default-setting")]
+        public async Task GetDefaultSetting(Request request, Response response)
+        {
+            await response.JsonResponse(200, new DefaultSettingResponse
+            {
+                status = 1,
+                start_time = Config.Config.Options.StartTime
+            });
+        }
+    }
+}
---
+++ ccxc-backend/Controllers/System/SystemRequestModels.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ccxc_backend.Controllers.System
+{
+    public class DefaultSettingResponse : BasicResponse
+    {
+        public long start_time { get; set; }
+    }
+}
