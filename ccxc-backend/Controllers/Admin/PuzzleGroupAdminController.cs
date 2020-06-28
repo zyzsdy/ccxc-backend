d63b45fb2dae23016f37258d638fb6721b9d5800
Ted Zyzsdy <zyzsdy@gmail.com>
2020-06-28 17:18:29 +0800

    题目分区后台管理

---
+++ ccxc-backend/Controllers/Admin/PuzzleGroupAdminController.cs
@@ -0,0 +1,114 @@
+﻿using Ccxc.Core.HttpServer;
+using ccxc_backend.DataModels;
+using ccxc_backend.DataServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ccxc_backend.Controllers.Admin
+{
+    [Export(typeof(HttpController))]
+    public class PuzzleGroupAdminController : HttpController
+    {
+        [HttpHandler("POST", "/admin/add-puzzle-group")]
+        public async Task AddPuzzleGroup(Request request, Response response)
+        {
+            var userSession = await CheckAuth.Check(request, response, AuthLevel.Organizer);
+            if (userSession == null) return;
+
+            var requestJson = request.Json<AddPuzzleGroupRequest>();
+
+            //判断请求是否有效
+            if (!Validation.Valid(requestJson, out string reason))
+            {
+                await response.BadRequest(reason);
+                return;
+            }
+
+            //插入新分区
+            var pgDb = DbFactory.Get<PuzzleGroup>();
+            var newPg = new puzzle_group
+            {
+                pg_name = requestJson.pg_name,
+                pg_desc = requestJson.pg_desc
+            };
+
+            await pgDb.SimpleDb.AsInsertable(newPg).ExecuteCommandAsync();
+            await pgDb.InvalidateCache();
+
+            await response.OK();
+        }
+
+        [HttpHandler("POST", "/admin/delete-puzzle-group")]
+        public async Task DeletePuzzleGroup(Request request, Response response)
+        {
+            var userSession = await CheckAuth.Check(request, response, AuthLevel.Organizer);
+            if (userSession == null) return;
+
+            var requestJson = request.Json<DeletePuzzleGroupRequest>();
+
+            //判断请求是否有效
+            if (!Validation.Valid(requestJson, out string reason))
+            {
+                await response.BadRequest(reason);
+                return;
+            }
+
+            //删除它
+            var pgDb = DbFactory.Get<PuzzleGroup>();
+            await pgDb.SimpleDb.AsDeleteable().Where(it => it.pgid == requestJson.pgid).ExecuteCommandAsync();
+            await pgDb.InvalidateCache();
+
+            await response.OK();
+        }
+
+        [HttpHandler("POST", "/admin/edit-puzzle-group")]
+        public async Task EditPuzzleGroup(Request request, Response response)
+        {
+            var userSession = await CheckAuth.Check(request, response, AuthLevel.Organizer);
+            if (userSession == null) return;
+
+            var requestJson = request.Json<EditPuzzleGroupRequest>();
+
+            //判断请求是否有效
+            if (!Validation.Valid(requestJson, out string reason))
+            {
+                await response.BadRequest(reason);
+                return;
+            }
+
+            //生成修改后对象
+            var updatePg = new puzzle_group
+            {
+                pgid = requestJson.pgid,
+                pg_name = requestJson.pg_name,
+                pg_desc = requestJson.pg_desc
+            };
+
+            var pgDb = DbFactory.Get<PuzzleGroup>();
+            await pgDb.SimpleDb.AsUpdateable(updatePg).ExecuteCommandAsync();
+            await pgDb.InvalidateCache();
+
+            await response.OK();
+        }
+
+        [HttpHandler("POST", "/admin/get-puzzle-group")]
+        public async Task GetAnnouncement(Request request, Response response)
+        {
+            var userSession = await CheckAuth.Check(request, response, AuthLevel.Organizer);
+            if (userSession == null) return;
+
+            var pgDb = DbFactory.Get<PuzzleGroup>();
+            var pgList = (await pgDb.SelectAllFromCache()).OrderBy(it => it.pgid);
+
+            await response.JsonResponse(200, new GetPuzzleGroupResponse
+            {
+                status = 1,
+                puzzle_group = pgList.ToList()
+            });
+        }
+    }
+}
---
+++ ccxc-backend/Controllers/Admin/PuzzleGroupAdminRequest.cs
@@ -0,0 +1,34 @@
+﻿using Ccxc.Core.HttpServer;
+using ccxc_backend.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ccxc_backend.Controllers.Admin
+{
+    public class AddPuzzleGroupRequest
+    {
+        [Required(Message = "分区名称不能为空")]
+        public string pg_name { get; set; }
+        public string pg_desc { get; set; }
+    }
+    
+    public class DeletePuzzleGroupRequest
+    {
+        public int pgid { get; set; }
+    }
+
+    public class EditPuzzleGroupRequest
+    {
+        public int pgid { get; set; }
+
+        [Required(Message = "分区名称不能为空")]
+        public string pg_name { get; set; }
+        public string pg_desc { get; set; }
+    }
+
+    public class GetPuzzleGroupResponse : BasicResponse
+    {
+        public List<puzzle_group> puzzle_group { get; set; }
+    }
+}
