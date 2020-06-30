58ee0889074cb6f263107fd7027b8ec99371d9ce
Ted Zyzsdy <zyzsdy@gmail.com>
2020-07-01 00:00:16 +0800

    增加题目管理

---
+++ ccxc-backend/Controllers/Admin/PuzzleAdminController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ccxc.Core.HttpServer;
+using ccxc_backend.DataModels;
+using ccxc_backend.DataServices;
+
+namespace ccxc_backend.Controllers.Admin
+{
+    [Export(typeof(HttpController))]
+    public class PuzzleAdminController : HttpController
+    {
+        [HttpHandler("POST", "/admin/add-puzzle")]
+        public async Task AddPuzzle(Request request, Response response)
+        {
+            var userSession = await CheckAuth.Check(request, response, AuthLevel.Organizer);
+            if (userSession == null) return;
+
+            var requestJson = request.Json<AddPuzzleRequest>();
+
+            //判断请求是否有效
+            if (!Validation.Valid(requestJson, out string reason))
+            {
+                await response.BadRequest(reason);
+                return;
+            }
+
+            //插入新题目
+            var puzzleDb = DbFactory.Get<Puzzle>();
+            var newPuzzle = new puzzle
+            {
+                pgid = requestJson.pgid,
+                type = requestJson.type,
+                title = requestJson.title,
+                content = requestJson.content,
+                image = requestJson.image,
+                html = requestJson.html,
+                answer_type = requestJson.answer_type,
+                answer = requestJson.answer
+            };
+
+            await puzzleDb.SimpleDb.AsInsertable(newPuzzle).ExecuteCommandAsync();
+            await puzzleDb.InvalidateCache();
+
+            await response.OK();
+        }
+
+        [HttpHandler("POST", "/admin/delete-puzzle")]
+        public async Task DeletePuzzle(Request request, Response response)
+        {
+            var userSession = await CheckAuth.Check(request, response, AuthLevel.Organizer);
+            if (userSession == null) return;
+
+            var requestJson = request.Json<DeletePuzzleRequest>();
+
+            //判断请求是否有效
+            if (!Validation.Valid(requestJson, out string reason))
+            {
+                await response.BadRequest(reason);
+                return;
+            }
+
+            //删除它
+            var puzzleDb = DbFactory.Get<Puzzle>();
+            await puzzleDb.SimpleDb.AsDeleteable().Where(it => it.pid == requestJson.pid).ExecuteCommandAsync();
+            await puzzleDb.InvalidateCache();
+
+            await response.OK();
+        }
+
+        [HttpHandler("POST", "/admin/edit-puzzle")]
+        public async Task EditPuzzle(Request request, Response response)
+        {
+            var userSession = await CheckAuth.Check(request, response, AuthLevel.Organizer);
+            if (userSession == null) return;
+
+            var requestJson = request.Json<EditPuzzleRequest>();
+
+            //判断请求是否有效
+            if (!Validation.Valid(requestJson, out string reason))
+            {
+                await response.BadRequest(reason);
+                return;
+            }
+
+            //生成修改后对象
+            var updatePuzzle = new puzzle
+            {
+                pid = requestJson.pid,
+                pgid = requestJson.pgid,
+                type = requestJson.type,
+                title = requestJson.title,
+                content = requestJson.content,
+                image = requestJson.image,
+                html = requestJson.html,
+                answer_type = requestJson.answer_type,
+                answer = requestJson.answer
+            };
+
+            var puzzleDb = DbFactory.Get<Puzzle>();
+            await puzzleDb.SimpleDb.AsUpdateable(updatePuzzle).ExecuteCommandAsync();
+            await puzzleDb.InvalidateCache();
+
+            await response.OK();
+        }
+
+        [HttpHandler("POST", "/admin/get-puzzle")]
+        public async Task GetPuzzle(Request request, Response response)
+        {
+            var userSession = await CheckAuth.Check(request, response, AuthLevel.Organizer);
+            if (userSession == null) return;
+
+            var puzzleDb = DbFactory.Get<Puzzle>();
+            var puzzleList = (await puzzleDb.SelectAllFromCache()).OrderBy(it => it.pgid).ThenBy(it => it.pid);
+
+            await response.JsonResponse(200, new GetPuzzleResponse
+            {
+                status = 1,
+                puzzle = puzzleList.ToList()
+            });
+        }
+    }
+}
---
+++ ccxc-backend/Controllers/Admin/PuzzleAdminRequest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ccxc.Core.HttpServer;
+using ccxc_backend.DataModels;
+
+namespace ccxc_backend.Controllers.Admin
+{
+    public class AddPuzzleRequest
+    {
+        public int pgid { get; set; }
+        public byte type { get; set; }
+
+        [Required(Message = "标题不能为空")]
+        public string title { get; set; }
+
+        public string content { get; set; }
+        public string image { get; set; }
+        public string html { get; set; }
+        public byte answer_type { get; set; }
+
+        [Required(Message = "答案不能为空")]
+        public string answer { get; set; }
+    }
+
+    public class DeletePuzzleRequest
+    {
+        public int pid { get; set; }
+    }
+
+    public class EditPuzzleRequest
+    {
+        public int pid { get; set; }
+        public int pgid { get; set; }
+        public byte type { get; set; }
+
+        [Required(Message = "标题不能为空")]
+        public string title { get; set; }
+
+        public string content { get; set; }
+        public string image { get; set; }
+        public string html { get; set; }
+        public byte answer_type { get; set; }
+
+        [Required(Message = "答案不能为空")]
+        public string answer { get; set; }
+    }
+
+    public class GetPuzzleResponse : BasicResponse
+    {
+        public List<puzzle> puzzle { get; set; }
+    }
+}
